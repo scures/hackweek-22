d48c5423cb27fe21260da8ef76eec4fa1fbcf5c8
scures <scurescu@suse.com>
2023-02-01 11:22:06 +0100

    Moves fetch to utils

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,9 @@
-use serde_json::Value;
-use std::env;
 mod utils;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let get_clusters = fetch("v1/management.cattle.io.clusters").await?;
+    let get_clusters = utils::fetch("v1/management.cattle.io.clusters").await?;
 
     println!("{}", get_clusters);
     Ok(())
-}
-
-async fn fetch(endpoint: &str) -> Result<Value, reqwest::Error> {
-    let rancher_url = env::var("RANCHER_URL").expect("Error: VAR_NAME not found");
-    let bearer_token = env::var("RANCHER_TOKEN").expect("Error: VAR_NAME not found");
-
-    print!("{}{}", rancher_url, bearer_token);
-
-    let url = format!("{}{}", rancher_url, endpoint);
-
-    let response = reqwest::Client::new()
-        .get(url)
-        .bearer_auth(bearer_token)
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    let parsed_json = utils::parse_json(&response).unwrap();
-    // Return the clusters as parsed json
-    Ok(parsed_json)
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,26 @@
 use serde_json::{Result as ResultJSON, Value};
+use std::env;
+
+pub async fn fetch(endpoint: &str) -> Result<Value, reqwest::Error> {
+    let rancher_url = env::var("RANCHER_URL").expect("Error: VAR_NAME not found");
+    let bearer_token = env::var("RANCHER_TOKEN").expect("Error: VAR_NAME not found");
+
+    print!("{}{}", rancher_url, bearer_token);
+
+    let url = format!("{}{}", rancher_url, endpoint);
+
+    let response = reqwest::Client::new()
+        .get(url)
+        .bearer_auth(bearer_token)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let parsed_json = parse_json(&response).unwrap();
+    // Return the clusters as parsed json
+    Ok(parsed_json)
+}
 
 pub fn parse_json(json_str: &str) -> ResultJSON<Value> {
     let value: Value = serde_json::from_str(json_str)?;
